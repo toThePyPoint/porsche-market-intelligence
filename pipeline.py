914c3650285ca5bfb64a68071df3bac12e6a3393
toThePyPoint <tothepypoint@gmail.com>
2026-09-12 12:13:10 +0200

    feat: add new pipeline layer

---
+++ pipeline.py
@@ -0,0 +1,25 @@
+from data_model import SearchAdvertData
+from listing_repository import ListingRepository
+from scraper_otomoto import OtomotoScraper
+
+
+class ListingService:
+    DB_NAME = 'test_db.db'
+
+    def __init__(self, test_mode = False):
+        self.repo = None
+        self.scraper = OtomotoScraper(test_mode=test_mode)
+
+    def identify_new_adverts(self):
+        pass
+
+    def process(self):
+        self.scraper.light_crawl()
+
+        self.repo = ListingRepository(self.scraper.searched_listings, self.DB_NAME)
+        self.repo.create_tables()
+        self.repo.insert_listings_to_db()
+
+        # TODO: Identify new orders
+
+        # TODO: Run Heavy crawling from scraper
