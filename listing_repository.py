59cc6aba1fb6555d3b8cda548a79d58f2669f6f3
toThePyPoint <tothepypoint@gmail.com>
2026-09-12 11:53:05 +0200

    fix: add try/finally syntax to ListingRepository.

--- listing_repository.py
+++ listing_repository.py
@@ -5,8 +5,11 @@ from data_model import SearchAdvertData
 
 
 class ListingRepository:
+    SNAPSHOTS_TABLE_NAME = 'listings_snapshots'
+    DETAILS_TABLE_NAME = 'listings_details'
+
     def __init__(self, listings: list[SearchAdvertData], db_name: str):
-        self.listings = listings
+        self.searched_listings = listings
         self.db_name = db_name
 
     def set_connection(self):
@@ -19,33 +22,46 @@ class ListingRepository:
     def create_tables(self):
         """Creates tables in database."""
         conn, cursor = self.set_connection()
-        cursor.execute(
-            '''CREATE TABLE IF NOT EXISTS listings_snapshots (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    advert_id TEXT NOT NULL,
-                    title TEXT NOT NULL,
-                    short_description TEXT NOT NULL,
-                    price INTEGER NOT NULL,
-                    currency TEXT NOT NULL,
-                    scraped_at DATETIME NOT NULL,
-                    url TEXT NOT NULL
-                )
-            '''
-        )
+        try:
+            cursor.execute(
+                f'''CREATE TABLE IF NOT EXISTS {self.SNAPSHOTS_TABLE_NAME} (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        advert_id TEXT NOT NULL,
+                        title TEXT NOT NULL,
+                        short_description TEXT NOT NULL,
+                        price INTEGER NOT NULL,
+                        currency TEXT NOT NULL,
+                        scraped_at DATETIME NOT NULL,
+                        url TEXT NOT NULL
+                    )
+                '''
+            )
 
-        conn.commit()
-        conn.close()
+            cursor.execute(
+                f'''CREATE TABLE IF NOT EXISTS {self.DETAILS_TABLE_NAME} (
+                        advert_id TEXT PRIMARY KEY,
+                        city TEXT NOT NULL,
+                        province TEXT NOT NULL
+                    )
+                '''
+            )
+
+            conn.commit()
+        finally:
+            conn.close()
 
     def insert_listings_to_db(self):
         conn, cursor = self.set_connection()
-        cursor.executemany(
-            '''INSERT INTO listings_snapshots (
-            advert_id, title, short_description, price, currency, scraped_at, url
+        try:
+            cursor.executemany(
+                f'''INSERT INTO {self.SNAPSHOTS_TABLE_NAME} (
+                advert_id, title, short_description, price, currency, scraped_at, url
+                )
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+                ''',
+                (astuple(item) for item in self.searched_listings)
             )
-            VALUES (?, ?, ?, ?, ?, ?, ?)
-            ''',
-            (astuple(item) for item in self.listings)
-        )
 
-        conn.commit()
-        conn.close()
+            conn.commit()
+        finally:
+            conn.close()
