caab015bc5a9f792390aa6b4cc44ece42543578d
toThePyPoint <tothepypoint@gmail.com>
2026-09-24 19:37:18 +0200

    add currency to vw_listings_lifecycle and add new view - vw_active_listings

--- database/views.sql
+++ database/views.sql
@@ -6,7 +6,8 @@ WITH lifecycle AS (
         MAX(snapshot_date) AS last_seen_at,
         COUNT(*) AS snapshots_count,
         MIN(price) AS min_price,
-        MAX(price) AS max_price
+        MAX(price) AS max_price,
+        currency
     FROM listings_snapshots
     GROUP BY advert_id
 )
@@ -24,6 +25,7 @@ SELECT
     first_snapshot.price AS initial_price,
     last_snapshot.price AS current_price,
     last_snapshot.price - first_snapshot.price AS price_change,
+    l.currency,
     ROUND(
         (last_snapshot.price - first_snapshot.price) * 100.0
         / first_snapshot.price,
@@ -36,3 +38,43 @@ JOIN listings_snapshots first_snapshot
 JOIN listings_snapshots last_snapshot
     ON last_snapshot.advert_id = l.advert_id
     AND last_snapshot.snapshot_date = l.last_seen_at;
+
+
+CREATE VIEW IF NOT EXISTS vw_active_listings AS
+SELECT
+    s.advert_id,
+    s.title,
+    s.price,
+    s.currency,
+    s.snapshot_date,
+    s.scraped_at,
+    s.url,
+
+    d.make,
+    d.model,
+    d.version,
+    d.generation,
+    d.year,
+    d.mileage,
+    d.engine_size_cm3,
+    d.engine_power_hp,
+    d.body_type,
+    d.gearbox,
+    d.fuel_type,
+    d.drive_type,
+    d.province,
+    d.city,
+    d.no_accident,
+    d.damaged,
+    d.seller_type,
+    d.dealer_type,
+    d.first_seen_at
+
+FROM listings_snapshots AS s
+LEFT JOIN listings_details AS d
+    ON s.advert_id = d.advert_id
+
+WHERE s.snapshot_date = (
+    SELECT MAX(snapshot_date)
+    FROM listings_snapshots
+);
